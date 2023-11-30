55747de1fd5b2744e7dcca1b90dee4cf96dd2013
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-11-30 13:56:39 -0800

    listen if no error when connecting

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const { connectToDb, getDb } = require("./db");
 
 //init app & middleware
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("app listening on port 3000");
+//db connection
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("app listening on port 3000");
+    });
+  }
 });
 
 // routes
